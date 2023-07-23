927b538e50d7e1b13e5947b92d17320e588c8af3
nayaablakhani <nayaablakhani94@gmail.com>
2023-07-23 17:20:16 +0530

    Added sound and copy paste feature to enhaance the project

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+
 const btn = document.querySelector("button");
 const heading = document.querySelector("h4");
+
+const soundBtn = document.querySelector(".sound");
+const copyBtn = document.querySelector(".copy");
+
+
+
 btn.addEventListener("click", getRandomJoke);
 function getRandomJoke() {
   const xml = new XMLHttpRequest();
@@ -10,3 +17,16 @@ function getRandomJoke() {
   };
   xml.send();
 }
+
+// SpeechSysthesisUtternce is a speech API that requests a speech request 
+soundBtn.addEventListener('click', () => {
+    let utternce = new SpeechSynthesisUtterance(`${heading.innerText}`)
+    speechSynthesis.speak(utternce);
+    //speak method of spechsynthesis speaks the text string
+});
+
+
+copyBtn.addEventListener('click', () => {
+    navigator.clipboard.writeText(heading.innerText);
+    //writeText function allows to copy text to your system clipboard
+})
